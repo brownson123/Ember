307b479c81db19b78553d62ee4e4bf24e0dcb1c3
Brownson Omotoye <omotoyeb@sheridancollege.ca>
2026-04-24 23:16:23 -0400

    successfullyimplemented the login and signup features using supabase

--- Ember/app/modal.tsx
+++ Ember/app/modal.tsx
@@ -8,8 +8,8 @@ export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
       <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
+      <Link href="/login" dismissTo style={styles.link}>
+        <ThemedText type="link">Back to login</ThemedText>
       </Link>
     </ThemedView>
   );
